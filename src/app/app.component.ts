1d8a6835d8743e4a571ed893c35facd247792368
tamyfabius <tamyfabius@gmail.com>
2023-12-07 14:17:44 +0100

    tttttt

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,197 +1,119 @@
-// Angular Core
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { PermissionService, UserService } from '@skynes/shared/utils/util-auth';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { DofTicketAttachmentModel, Exemption } from '@skynes/dof/domain';
+import { LabelValueModel } from '@skynes/shared/su7-models';
 
-// Libs
-import {
-  setGlobalConfigRxweb,
-  setItemInLocalStorage,
-} from '@skynes/shared/utils/util-common';
-import { LayoutService } from '@sakai-layout-v2';
-import { MenuItem } from 'primeng/api';
-import { forkJoin, Subscription } from 'rxjs';
-import { AuthDofService } from '../services/auth-dof.service';
-import {
-  PermissionManagerService,
-  PermissionTypeEnum,
-} from '@skynes/shared/utils/util-permission-manage';
-import { TranslateService } from '@ngx-translate/core';
-import { DofTicketActionRequiredFacade } from '@skynes/dof/domain';
-import { DofNotificationContentComponent } from '@skynes/dof/firewall-rules-request';
-
+/**
+ * @class AttachmentComponent
+ * @description Attachment component
+ */
 @Component({
-  selector: 'dof-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: 'dofin-attachment',
+  templateUrl: './attachment.component.html',
+  styleUrls: ['./attachment.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  title = 'portail-dof-v0';
-
-  private _sub = new Subscription();
-
-  constructor(
-    private _authService: AuthDofService,
-    private _dofTicketActionRequiredFacade: DofTicketActionRequiredFacade,
-    private _userService: UserService,
-    private _layoutService: LayoutService,
-    private _permissionService: PermissionService,
-    private _permissionManagerService: PermissionManagerService,
-    private _translate: TranslateService
-  ) {
-    const browserLang = _translate.getBrowserLang();
-    _translate.use(browserLang?.match('/en|fr/') ? browserLang : 'fr');
-  }
+export class AttachmentComponent {
+  /**
+   * @description Input dof ticket attachment
+   */
+  @Input() dofTicketAttachment: DofTicketAttachmentModel =
+    new DofTicketAttachmentModel();
 
   /**
-   * @function _configRxWeb
-   * @private
-   * @description set global config Rxweb
+   * @description Input styleClass
    */
-  private static _configRxWeb(): void {
-    setGlobalConfigRxweb();
-  }
-
-  ngOnInit(): void {
-    AppComponent._configRxWeb();
-    this._initLoadData();
-    this._configLayout();
-  }
-
-  ngOnDestroy(): void {
-    this._sub.unsubscribe();
-  }
+  @Input() styleClass = '';
 
   /**
-   * @function _configLayout
-   * @private
-   * @description set config layout
+   * @description Input send attachent loading
    */
-  private _configLayout(): void {
-    const env = this._layoutService.config.env;
-    let logoDofin = '';
-    if (env === 'production') {
-      logoDofin = 'assets/images/logo-dofin-prod.svg';
-    } else if (env === 'bench') {
-      logoDofin = 'assets/images/logo-dofin-bench.svg';
-    } else {
-      logoDofin = 'assets/images/logo-dofin-dev.svg';
+  @Input() sendAttachmentLoading = false;
+
+  /**
+   * @description Input send attachent loading
+   */
+
+  /**
+   * @description Display send attachment button
+   */
+  @Input() displaySendAttachmentButton = true;
+
+  /**
+   * @description Upload file event emitter
+   */
+  @Output() uploadFile = new EventEmitter<DofTicketAttachmentModel>();
+
+  /**
+   * @description Add exemption ticket number
+   */
+  @Output() addExemptionTicket = new EventEmitter<string>();
+
+  /**
+   * @description Attachment to send
+   */
+  attachmentToSend: DofTicketAttachmentModel = new DofTicketAttachmentModel();
+  readonly ExemptionEnum = Exemption;
+  exemptionConf: {
+    exemptionRequested: string;
+    exemptionSelected: string | null;
+  } = {
+    exemptionRequested: 'false',
+    exemptionSelected: null,
+  };
+  exemptionRequestedOptions: LabelValueModel[] = [
+    { label: 'Yes', value: 'true' },
+    { label: 'No', value: 'false' },
+  ];
+  exemptionOptions: LabelValueModel[] = [
+    this.ExemptionEnum.exemptionTicket,
+    this.ExemptionEnum.drive,
+  ];
+
+  constructor() {}
+
+  /**
+   * @function onBuildDataToSend
+   * @param type
+   * @param event
+   * @description Build data to send event callback
+   */
+  onBuildDataToSend(type: string, event: any): void {
+    this.attachmentToSend.ticket_id = this.dofTicketAttachment.ticket_id;
+    if (type === 'architecture') {
+      this.attachmentToSend.architecture_diagram_file = event?.currentFiles[0];
     }
-    const logoBPCE = 'assets/images/logo-bpce-it.png';
-    const logos = {
-      logoDofin,
-      logoBPCE,
-    };
-    this._layoutService.initConfigLayout(this._getMenuItems(), logos);
+    if (type === 'exemption') {
+      this.attachmentToSend.exemption_file = event?.currentFiles[0];
+    }
   }
 
   /**
-   * @function _buildMenu
-   * @private
-   * @return {MenuItem[]}
-   * @description build menu items
+   * @function onUploadFile
+   * @description Upload file event callback
    */
-  private _getMenuItems(): MenuItem[] {
-    return this._authService.isAuthenticated() &&
-      this._permissionService.hasPermission()
-      ? [
-          {
-            label: '',
-            items: [
-              {
-                label: 'Dashboard',
-                icon: 'dofin-icon dofin-icon-admin-view',
-                iconClass: 'layout-menuitem-icon',
-                routerLink: ['/dof/dashboard'],
-                visible:
-                  this._permissionService.hasPermission() &&
-                  this._permissionManagerService.isGranted(
-                    PermissionTypeEnum.READ,
-                    'permission'
-                  ),
-              },
-              {
-                label: 'KPI',
-                icon: 'pi pi-percentage',
-                iconClass: 'layout-menuitem-icon',
-                routerLink: ['/dof/kpis'],
-                visible:
-                  this._permissionService.hasPermission() &&
-                  this._permissionManagerService.isGranted(
-                    PermissionTypeEnum.READ,
-                    'kpis'
-                  ),
-              },
-            ],
-          },
-          {
-            label: 'Firewall Request',
-            items: [
-              {
-                label: 'Create New Request',
-                icon: 'dofin-icon dofin-icon-file-add',
-                routerLink: ['/dof/firewall-rules-request'],
-                iconClass: 'layout-menuitem-icon',
-                visible:
-                  this._permissionService.hasPermission() &&
-                  this._permissionManagerService.isGranted(
-                    PermissionTypeEnum.CREATE,
-                    'dofticket'
-                  ),
-              },
-              {
-                label: 'My Team Requests',
-                icon: 'dofin-icon dofin-icon-users',
-                routerLink: ['/dof/firewall-rules-request/request-list'],
-                iconClass: 'layout-menuitem-icon',
-                visible:
-                  this._permissionService.hasPermission() &&
-                  this._permissionManagerService.isGranted(
-                    PermissionTypeEnum.READ,
-                    'dofticket'
-                  ),
-              },
-              {
-                label: 'Admin view',
-                icon: 'dofin-icon dofin-icon-bookmark-star',
-                routerLink: ['/dof/firewall-rules-request/admin-view'],
-                iconClass: 'layout-menuitem-icon',
-                visible:
-                  this._permissionService.hasPermission() &&
-                  this._permissionManagerService.isGranted(
-                    PermissionTypeEnum.READ,
-                    'dofticket'
-                  ),
-              },
-            ],
-            visible:
-              this._permissionService.hasPermission() &&
-              this._permissionManagerService.isGranted(
-                PermissionTypeEnum.READ,
-                'dofticket'
-              ),
-          },
-        ]
-      : [];
+  onUploadFile(): void {
+    this.uploadFile.emit(this.attachmentToSend);
   }
 
-  private _initLoadData(): void {
-    const fk = forkJoin([
-      this._userService.getSkynesUsersMe(),
-      this._dofTicketActionRequiredFacade.getDofTicketActionRequired(),
-    ]);
-    this._sub.add(
-      fk.subscribe(([skynesUserData, actionRequiredData]) => {
-        setItemInLocalStorage('skynesUsersMe', JSON.stringify(skynesUserData));
-        this._permissionService.setPermission(skynesUserData.user_permissions);
-        const notificationInput = {
-          actionRequiredList: actionRequiredData,
-        };
-        this._layoutService.setNotificationConfig(
-          DofNotificationContentComponent,
-          actionRequiredData,
-          notificationInput
-        );
-      })
-    );
+  /**
+   * @function onExemptionRequested
+   * @param exemptionRequested
+   */
+  onExemptionRequested(exemptionRequested: string): void {
+    this.exemptionConf = {
+      exemptionRequested,
+      exemptionSelected: null,
+    };
+    console.log('onExemptionRequested - exemptionConf', this.exemptionConf);
+  }
+
+  /**
+   * @function onExemptionOptions
+   * @param exemptionSelected
+   */
+  onExemptionOptions(exemptionSelected: string): void {
+    this.exemptionConf = {
+      ...this.exemptionConf,
+      exemptionSelected,
+    };
   }
 }
